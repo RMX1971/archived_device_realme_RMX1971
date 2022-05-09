4e3b73c28f4d0c88456166a97f3e47cb7ee84d06
Naufal Altaf <nao@altaf.xyz>
2022-05-09 07:18:29 +0700

    RMX1971: Declare notch presence
    Disables options that were not fully compatible due to the notch such as center clock and traffic monitor.
    
    Change-Id: I2bed7333a5f357df691e4cc06c6253956305e79c

--- device.mk
+++ device.mk
@@ -35,7 +35,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Soong namespace
 PRODUCT_SOONG_NAMESPACES += \
