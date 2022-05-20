bc3c34b5f15719c292d3d232e435e57c98000066
Naufal Altaf <nao@altaf.xyz>
2022-05-20 21:49:20 +0900

    RMX1971: Start building vendor image
    Change-Id: I79adb79ccd12fc1f54d9e4fd30a737d0f3c6ff46

--- lineage_RMX1971.mk
+++ lineage_RMX1971.mk
@@ -26,6 +26,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
+# Inherit common AOSP vendor configurations
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+
 # Inherit common LineageOS configurations
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
